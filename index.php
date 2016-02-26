04b096c2cf0fc94821bd72e1dcb17933036240c9
Jag Uppal <jag4091@gmail.com>
2016-02-26 16:24:09 +0000

    updated paths for includes and assets to point to the dist folder rather than src folder

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php include 'src/components/head/head.php' ?>
+<?php include 'dist/includes/head/head.php' ?>
 
-<?php include 'src/components/navigation/navigation.php' ?>
+<?php include 'dist/includes/navigation/navigation.php' ?>
 
-<?php include 'src/components/footer-scripts/footer-scripts.php' ?>
+<?php include 'dist/includes/footer-scripts/footer-scripts.php' ?>
--- src/components/footer-scripts/footer-scripts.php
+++ src/components/footer-scripts/footer-scripts.php
@@ -1,8 +1,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/dist/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="dist/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
 
-        <script type="text/javascript" src="/boilerplate/dist/js/plugins.js"></script>
-        <script type="text/javascript" src="/boilerplate/dist/js/app.js"></script>
+        <script type="text/javascript" src="dist/js/plugins.js"></script>
+        <script type="text/javascript" src="dist/js/app.js"></script>
 
     </body>
 
