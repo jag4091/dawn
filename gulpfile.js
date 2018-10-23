9889b8aa23cb6a1a9f1be04fba03c9d5cce8f24a
Jag Uppal <jag4091@gmail.com>
2018-10-23 17:05:16 +0100

    added light grey and updated gulpfile with hosts

--- gulpfile.js
+++ gulpfile.js
@@ -172,7 +172,7 @@ gulp.task('js-watch', ['jshint', 'build-js'], browserSync.reload);
 gulp.task('serve', ['build-bundle', 'jshint', 'build-js', 'build-plugins', 'build-includes'], function() {
 
     browserSync.init({
-        proxy: "localhost:8888/dawn/"
+        proxy: "dev.dawn" // make this what you have declared in your hosts
     });
 
     gulp.watch([input.scss, 'src/components/**/*.scss'], ['build-bundle']);
